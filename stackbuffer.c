c320d3d17b477b16e8a4153e249c0d5bf16cb4c5
tseto93 <tseto03@gmail.com>
2017-07-12 13:28:11 -0700

    Add files via upload

---
+++ stackbuffer.c
@@ -0,0 +1,25 @@
+#include <strings.h>
+#include <stdio.h>
+
+#define MAX_SIZE 128
+
+void doMemCpy(char *buf, char *in, int chars)
+{ 
+    memcpy(buf, in, chars); 
+}
+
+int main()
+{ 
+    char buf[64]; 
+    char in[MAX_SIZE]; 
+    int bytes;
+
+    printf("Enter buffer contents:\n"); 
+    read(0, in, MAX_SIZE-1); 
+    printf("Bytes to copy:\n"); 
+    scanf("%d", &bytes);
+
+    doMemCpy(buf, in, bytes);
+
+    return(0); 
+} 
